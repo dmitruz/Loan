9368c7db82c3420b4f273e353755363b22df3a91
dmitruz <dmitruz970@gmail.com>
2023-04-09 12:30:06 +0300

    popup block in slide 3 done

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -5,13 +5,29 @@ export default class Slider {
     this.btns = document.querySelectorAll(btns);
     this.slideIndex = 1;
   }
+
   showSlides(n) {
     if (n > this.slides.length) {
       this.slideIndex = 1;
     }
+
     if (n < 1) {
       this.slideIndex = this.slides.length;
     }
+
+    try {
+      this.hanson.style.opacity = '0';
+
+      if (n == 3) {
+        this.hanson.classList.add('animated');
+        setTimeout(() => {
+          this.hanson.style.opacity = '1';
+          this.hanson.classList.add('slideInUp');
+        }, 3000);
+      } else {
+        this.hanson.classList.remove('slideInUp');
+      }
+    } catch (e) {}
 
     this.slides.forEach(slide => {
       slide.style.display = 'none';
@@ -25,6 +41,10 @@ export default class Slider {
   }
 
   render() {
+    try {
+      this.hanson = document.querySelector('.hanson');
+    } catch (e) {}
+
     this.btns.forEach(item => {
       item.addEventListener('click', () => {
         this.plusSlides(1);
@@ -36,6 +56,7 @@ export default class Slider {
         this.showSlides(this.slideIndex);
       });
     });
+
     this.showSlides(this.slideIndex);
   }
 }
