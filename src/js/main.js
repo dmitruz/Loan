e434168843f265b90d977eb9d9706900bda1fe83
dmitruz <dmitruz970@gmail.com>
2023-04-12 12:54:13 +0300

    update mainJS

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,14 @@
 import Slider from './modules/slider/slider-main';
 import MiniSlider from './modules/slider/slider-mini';
 import VideoPlayer from './modules/playVideo';
+import Difference from './modules/difference';
+import Form from './modules/forms';
+import ShowInfo from './modules/showInfo';
+import Download from './modules/download';
 
 window.addEventListener('DOMContentLoaded', () => {
-  const slider = new Slider({ btns: '.next', container: '.page' });
-  slider.render();
+  const modulePageSlider = new MainSlider({ container: '.moduleapp', btns: '.next' });
+  modulePageSlider.render();
 
   const showUpSlider = new MiniSlider({
     container: '.showup__content-slider',
@@ -25,14 +29,13 @@ window.addEventListener('DOMContentLoaded', () => {
   });
   modulesSlider.init();
 
-  const feedSlider = new MiniSlider({
-    container: '.feed__slider',
-    prev: '.feed__slider .slick-prev',
-    next: '.feed__slider .slick-next',
-    activeClass: 'feed__item-active',
-  });
-  feedSlider.init();
+  new VideoPlayer('.showup .play', '.overlay').init();
+  new VideoPlayer('.module__video-item .play', '.overlay').init();
 
-  const player = new VideoPlayer('.showup .play', '.overlay');
-  player.init();
+  new Difference('.officerold', '.officernew', '.officer__card-item').init();
+  new Form('.form').init();
+
+  new ShowInfo('.plus__content').init();
+
+  new Download('.download').init();
 });
